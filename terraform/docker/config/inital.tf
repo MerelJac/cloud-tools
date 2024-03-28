ba0a1097493afddb9abe9313635d3c45d1d0238e
Merel Jacobs <merel.burleigh@gmail.com>
2024-03-27 18:52:04 -0700

    inital.tf edits in terraform

--- terraform/docker/config/inital.tf
+++ terraform/docker/config/inital.tf
@@ -1,20 +1,9 @@
-module "webserver_cluster" {
-  source = "../infra" 
+resource "null_resource" "ConfigureAnsibleLabelVariable" {
+  provisioner "local-exec" {
+    command = "echo HELLO_WORLD"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${var.docker-droplet.ipv4_address}"
+  }
 }
-
-resource "configured_droplet" "docker" {
-
-  provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${webserver_cluster.ipv4_address},' --private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}' ansible/wait-for-init.yml"
-  }
-
-  provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${webserver_cluster.ipv4_address},' --private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}' ansible/install-docker.yml"
-  }
-
-  provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${webserver_cluster.ipv4_address},' --private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}' ansible/run-compose-file.yml"
-  }
-
-}
-
