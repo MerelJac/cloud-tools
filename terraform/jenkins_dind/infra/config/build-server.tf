9e3d12124b48b1a9d7e71e3e8825673afe53e5e4
Merel Jacobs <merel.burleigh@gmail.com>
2024-04-09 20:48:30 -0700

    adjust dns

--- terraform/jenkins_dind/infra/config/build-server.tf
+++ terraform/jenkins_dind/infra/config/build-server.tf
@@ -1,11 +1,26 @@
+resource "random_uuid" "force_config" {}
+
 resource "null_resource" "jenkins_droplet" {
+  triggers = {
+    reconfigure = "${random_uuid.force_config.result}"
+  }
+
   provisioner "local-exec" {
     command = join(" ", [
-                "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook",
-                "-u root -i '${module.do.raw_droplet.ipv4_address},'",
-                "--private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}'",
-                "ansible/install-docker.yml"
-              ])
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook",
+      "-u root -i '${module.do.raw_droplet.ipv4_address},'",
+      "--private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}'",
+      "ansible/install-docker.yml"
+    ])
+  }
+
+  provisioner "local-exec" {
+    command = join(" ", [
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook",
+      "-u root -i '${module.do.raw_droplet.ipv4_address},'",
+      "--private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}'",
+      "ansible/run-compose-project.yml"
+    ])
   }
 }
 
