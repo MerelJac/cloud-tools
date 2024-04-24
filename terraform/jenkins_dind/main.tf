e53338a22f4fe6abd7aaf41117f9865ec8f6dad3
Merel Jacobs <merel.burleigh@gmail.com>
2024-04-23 19:00:48 -0700

    add dns info

--- terraform/jenkins_dind/main.tf
+++ terraform/jenkins_dind/main.tf
@@ -5,6 +5,13 @@ module "droplet" {
   pub_key      = var.pub_key
 }
 
+module "dns" {
+  source       = "./infra/dns"
+  do_token     = var.do_token
+  pvt_key      = var.pvt_key
+  pub_key      = var.pub_key
+}
+
 resource "null_resource" "MainTF" {
   provisioner "local-exec" {
     command = "echo Finished provisioning."
