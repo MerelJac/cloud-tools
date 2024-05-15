a5a1c0af35bc8434322adae205f7be09c4f68441
Merel Jacobs <merel.burleigh@gmail.com>
2024-05-14 18:03:41 -0700

    changes

--- terraform/jenkins_dind/infra/dns/build-server.tf
+++ terraform/jenkins_dind/infra/dns/build-server.tf
@@ -12,6 +12,17 @@ resource "digitalocean_record" "web" {
   value  = "${module.server.server.ipv4_address}"
 }
 
+resource "digitalocean_record" "service" {
+  domain = "dops.stairways.ai"
+  type   = "A"
+  name   = "traefik.mjs"
+  value  = "${module.server.server.ipv4_address}"
+}
+
 output "web_alias" {
   value = digitalocean_record.web
+}
+
+output "service_alias" {
+  value = digitalocean_record.service
 }
