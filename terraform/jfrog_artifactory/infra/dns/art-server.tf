955a61f747e3c59e39433ca5bc60485a9945dc16
Merel Jacobs <merel.burleigh@gmail.com>
2024-06-04 19:06:06 -0700

    jfrog init

--- terraform/jfrog_artifactory/infra/dns/art-server.tf
+++ terraform/jfrog_artifactory/infra/dns/art-server.tf
@@ -5,13 +5,35 @@ module "server" {
   pub_key      = var.pub_key
 }
 
-resource "digitalocean_record" "web" {
+resource "digitalocean_record" "artifactory" {
   domain = "dops.stairways.ai"
   type   = "A"
   name   = "artifactory.mjs"
   value  = "${module.server.server.ipv4_address}"
 }
 
-output "web_alias" {
-  value = digitalocean_record.web
+output "art_alias" {
+  value = digitalocean_record.artifactory
+}
+
+resource "digitalocean_record" "jfrog" {
+  domain = "dops.stairways.ai"
+  type   = "A"
+  name   = "jfrog.mjs"
+  value  = "${module.server.server.ipv4_address}"
+}
+
+output "jfrog_alias" {
+  value = digitalocean_record.jfrog
+}
+
+resource "digitalocean_record" "art-traefik" {
+  domain = "dops.stairways.ai"
+  type   = "A"
+  name   = "art-traefik.mjs"
+  value  = "${module.server.server.ipv4_address}"
+}
+
+output "art-traefik_alias" {
+  value = digitalocean_record.art-traefik
 }
