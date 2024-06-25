029de51219dd813acaf696150c839b6498ab1ef1
Andrew Porter <swissarmybud@gmail.com>
2024-06-24 22:05:06 -0700

    Bumps the droplet capacity for JFrog

--- terraform/jfrog_artifactory/infra/do/droplet.tf
+++ terraform/jfrog_artifactory/infra/do/droplet.tf
@@ -2,7 +2,7 @@ resource "digitalocean_droplet" "droplet" {
     image = "debian-12-x64"
     name = "jfrog-art-server"
     region = "sfo3"
-    size = "s-1vcpu-2gb"
+    size = "s-2vcpu-4gb"
     tags = ["build", "server", "jfrog", "artifactory"]
     ssh_keys = [
       data.digitalocean_ssh_key.mj-sandbox.id, data.digitalocean_ssh_key.ap-sandbox.id
